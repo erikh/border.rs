8086ac2b3d7232e599ac3d4521fe7e8ebf995559
Erik Hollensbe <git@hollensbe.org>
2023-05-10 16:11:17 -0700

    temp
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/config.rs
+++ src/config.rs
@@ -1,34 +1,102 @@
 #![allow(dead_code)]
-use crate::record_type::{ToRecord, NS, SOA};
+use crate::record_type::{RecordValue, NS, SOA};
 use josekit::jwk::Jwk;
+use serde::{de::Visitor, ser::SerializeMap, Deserialize, Serialize};
 use std::net::{IpAddr, SocketAddr};
 use url::Url;
 
+#[derive(Serialize, Deserialize)]
 pub(crate) struct Config {
     auth_key: Jwk,
     listen: ListenConfig,
     peers: Vec<Peer>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub(crate) struct ListenConfig {
     dns: SocketAddr,
     control: SocketAddr,
 }
 
+#[derive(Serialize, Deserialize)]
 pub(crate) struct Peer {
     ips: Vec<IpAddr>,
     control_server: Url,
     key: Jwk,
 }
 
+#[derive(Serialize, Deserialize)]
 pub(crate) struct Zone {
     soa: SOA,
     ns: NS,
-    records: Vec<Record<dyn ToRecord>>,
+    records: Vec<Record>,
 }
 
-pub(crate) struct Record<T: ToRecord + ?Sized> {
-    typ: String,
+pub(crate) struct Record {
     name: String,
-    value: Box<T>,
+    value: RecordValue,
+}
+
+impl Default for Record {
+    fn default() -> Self {
+        Self {
+            name: String::default(),
+            value: RecordValue::NULL,
+        }
+    }
+}
+
+impl Serialize for Record {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(3))?;
+
+        map.serialize_entry("type", self.value.record_type());
+        map.serialize_entry("name", &self.name);
+        map.serialize_entry("value", &self.value.record_value());
+        map.end()
+    }
+}
+
+struct RecordVisitor;
+
+impl<'de> Visitor<'de> for RecordVisitor {
+    type Value = Record;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("expecting a record entry")
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::MapAccess<'de>,
+    {
+        let mut rec = Record::default();
+
+        while let Some(key) = map.next_key()? {
+            match key {
+                "name" => rec.name = map.next_value()?,
+                "value" => rec.value = map.next_value()?,
+                _ => {
+                    return Err(serde::de::Error::unknown_field(
+                        key,
+                        &["type", "name", "value"],
+                    ))
+                }
+            }
+        }
+
+        Ok(rec)
+    }
+}
+
+impl<'de> Deserialize<'de> for Record {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_map(RecordVisitor)
+    }
 }
--- src/record_type.rs
+++ src/record_type.rs
@@ -1,10 +1,81 @@
 #![allow(dead_code)]
+use anyhow::anyhow;
+use serde::{de::Visitor, Deserialize, Serialize};
 use std::net::IpAddr;
+
+#[derive(Serialize)]
+pub enum RecordValue {
+    A(Box<A>),
+    TXT(Box<TXT>),
+    NULL,
+}
+
+impl RecordValue {
+    pub fn record_type(&self) -> &str {
+        match self {
+            Self::A(_) => "a",
+            Self::TXT(_) => "txt",
+            Self::NULL => "null",
+        }
+    }
+
+    pub fn record_value(self) -> Option<Box<dyn ToRecord>> {
+        match self {
+            Self::A(a) => Some(a),
+            Self::TXT(txt) => Some(txt),
+            Self::NULL => None,
+        }
+    }
+
+    pub fn from_value(typ: &str, value: Box<dyn ToRecord>) -> Result<Self, anyhow::Error> {
+        match typ {
+            "a" | "A" => Ok(Self::A(value)),
+            "txt" | "TXT" => Ok(Self::TXT(value)),
+            _ => Err(anyhow!("invalid record type")),
+        }
+    }
+}
+
+impl Default for RecordValue {
+    fn default() -> Self {
+        Self::NULL
+    }
+}
+
+struct RecordValueVisitor;
+
+impl<'de> Visitor<'de> for RecordValueVisitor {
+    type Value = RecordValue;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("expected record information")
+    }
+
+    // fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    // where
+    //     A: serde::de::MapAccess<'de>,
+    // {
+    // }
+}
+
+impl<'de> Deserialize<'de> for RecordValue {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_map(RecordValueVisitor)
+    }
+}
+
+fn default_ttl() -> Option<u32> {
+    Some(30)
+}
 
 pub trait ToRecord {
     fn to_record(&self);
 }
 
+#[derive(Serialize, Deserialize, Default)]
 pub(crate) struct SOA {
     domain: String,
     admin: String,
@@ -19,18 +90,22 @@ impl ToRecord for SOA {
     fn to_record(&self) {}
 }
 
+#[derive(Serialize, Deserialize, Default)]
 pub(crate) struct NS {
     servers: Vec<String>,
-    ttl: u32,
+    #[serde(skip_serializing_if = "Option::is_none", default = "default_ttl")]
+    ttl: Option<u32>,
 }
 
 impl ToRecord for NS {
     fn to_record(&self) {}
 }
 
+#[derive(Serialize, Deserialize, Default)]
 pub(crate) struct A {
     addresses: Vec<IpAddr>,
-    ttl: u32,
+    #[serde(skip_serializing_if = "Option::is_none", default = "default_ttl")]
+    ttl: Option<u32>,
     // FIXME healthcheck
 }
 
@@ -38,11 +113,20 @@ impl ToRecord for A {
     fn to_record(&self) {}
 }
 
+#[derive(Serialize, Deserialize, Default)]
 pub(crate) struct TXT {
     value: Vec<String>,
-    ttl: u32,
+    #[serde(skip_serializing_if = "Option::is_none", default = "default_ttl")]
+    ttl: Option<u32>,
 }
 
 impl ToRecord for TXT {
+    fn to_record(&self) {}
+}
+
+#[derive(Serialize, Deserialize, Default)]
+pub(crate) struct NULL {}
+
+impl ToRecord for NULL {
     fn to_record(&self) {}
 }
