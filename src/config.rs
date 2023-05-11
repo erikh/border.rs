86848c8987106e7a21ed511e8b706f5a261d1176
Erik Hollensbe <git@hollensbe.org>
2023-05-11 11:31:26 -0700

    import trust-dns, serde for dns names
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,12 @@
 #![allow(dead_code)]
 use crate::record_type::{RecordType, NS, SOA};
 use josekit::jwk::Jwk;
-use serde::{Deserialize, Serialize};
+use serde::{de::Visitor, Deserialize, Serialize};
 use std::{
     collections::BTreeMap,
     net::{IpAddr, SocketAddr},
 };
+use trust_dns_server::proto::rr::Name;
 use url::Url;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -39,6 +40,47 @@ pub struct Zone {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Record {
-    name: String,
+    name: DNSName,
     record: RecordType,
+}
+
+#[derive(Debug)]
+pub struct DNSName(Name);
+
+impl Serialize for DNSName {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.0.to_string())
+    }
+}
+
+struct DNSNameVisitor;
+
+impl Visitor<'_> for DNSNameVisitor {
+    type Value = DNSName;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("expecting a DNS name")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(DNSName(match Name::parse(v, None) {
+            Ok(res) => res,
+            Err(e) => return Err(serde::de::Error::custom(e)),
+        }))
+    }
+}
+
+impl<'de> Deserialize<'de> for DNSName {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(DNSNameVisitor)
+    }
 }
