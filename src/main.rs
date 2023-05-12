a704e038587888827b1b9d5fd31fc919915044a5
Erik Hollensbe <git@hollensbe.org>
2023-05-12 13:14:07 -0700

    Start of a command framework
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
+use std::path::PathBuf;
+
 use border::config::Config;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(
+    author = "Erik Hollensbe <erik+github@hollensbe.org",
+    about = "Generate a tree of documents for testing DID parser compliance"
+)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(name = "config-check")]
+    ConfigCheck { filename: PathBuf },
+}
 
 fn main() -> Result<(), anyhow::Error> {
-    let mut f = std::fs::OpenOptions::new();
-    f.read(true);
-    let io = f.open("example.yaml")?;
-    let res: Config = serde_yaml::from_reader(io)?;
-    println!("{:?}", res);
+    let args = Args::parse();
+
+    match args.command {
+        Commands::ConfigCheck { filename } => {
+            let mut f = std::fs::OpenOptions::new();
+            f.read(true);
+            let io = f.open(filename)?;
+            let _: Config = serde_yaml::from_reader(io)?;
+            println!("Configuration Parsed OK");
+        }
+    }
+
     Ok(())
 }
