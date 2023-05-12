34363f0c74da649800af8568d973476eb0309243
Erik Hollensbe <git@hollensbe.org>
2023-05-12 13:26:17 -0700

    key generation
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use border::config::Config;
 use clap::{Parser, Subcommand};
+use josekit::{jwe::alg::aeskw::AeskwJweAlgorithm, jwk::Jwk};
 
 #[derive(Parser, Debug)]
 #[command(
@@ -17,20 +18,43 @@ struct Args {
 enum Commands {
     #[command(name = "config-check")]
     ConfigCheck { filename: PathBuf },
+    #[command(name = "key-generate")]
+    KeyGenerate {
+        #[arg(name = "Key ID (used for peer name in some cases)")]
+        peer_name: String,
+    },
 }
 
-fn main() -> Result<(), anyhow::Error> {
+type CommandResult = Result<(), anyhow::Error>;
+
+fn main() -> CommandResult {
     let args = Args::parse();
 
     match args.command {
-        Commands::ConfigCheck { filename } => {
-            let mut f = std::fs::OpenOptions::new();
-            f.read(true);
-            let io = f.open(filename)?;
-            let _: Config = serde_yaml::from_reader(io)?;
-            println!("Configuration Parsed OK");
-        }
+        Commands::ConfigCheck { filename } => check_config(filename),
+        Commands::KeyGenerate { peer_name } => generate_key(peer_name),
     }
+}
+
+fn check_config(filename: PathBuf) -> CommandResult {
+    let mut f = std::fs::OpenOptions::new();
+    f.read(true);
+    let io = f.open(filename)?;
+    let _: Config = serde_yaml::from_reader(io)?;
+    println!("Configuration Parsed OK");
+
+    Ok(())
+}
+
+fn generate_key(peer_name: String) -> CommandResult {
+    let mut jwk = Jwk::generate_oct_key(255)?;
+    jwk.set_algorithm(AeskwJweAlgorithm::A256kw.name());
+    jwk.set_key_id(peer_name);
+
+    println!();
+    println!("# Paste this key into your configuration where you need an encryption key.");
+    println!("# Indentation is important!");
+    println!("{}", serde_yaml::to_string(&jwk)?);
 
     Ok(())
 }
